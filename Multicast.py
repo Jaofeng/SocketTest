578ee9daa9ee56953227e68f38eea90b35f36e1a
怪頭 <jaofeng.chen@gmail.com>
2019-04-27 12:26:06 +0800

    Update

--- Multicast.py
+++ Multicast.py
@@ -7,35 +7,37 @@ from jfNet import *
 
 _rcv = None
 _snd = None
+_counter = 0
+
 
 def toHexStr(arr):
     return ' '.join('{:02X}'.format(x) for x in arr)
 
+
 def onStarted(*args):
-    if isinstance(args[0], jcr.CastReceiver):
-        print(' -> Multicast Receiver Started...')
-        # if len(_rcv.groups) != 0:
-        #     print('     -> {}'.format(', '.join(_rcv.groups)))
-    elif isinstance(args[0], jcr.CastSender):
-        print(' -> Multicast Sender Started...')
+    print(' -> Multicast Receiver Started...')
+
 
 def onStoped(*args):
-    if isinstance(args[0], jcr.CastReceiver):
-        print('  -> Multicast Receiver Stoped...')
-    elif isinstance(args[0], jcr.CastSender):
-        print('  -> Multicast Sender Stoped...')
+    print('  -> Multicast Receiver Stoped...')
+
 
 def onJoinedGroup(*args):
     ip = args[1]
     print(' -> Joined {}'.format(ip))
 
+
 def onReceived(*args):
+    global _counter
+    _counter += 1
     ipL, portL = args[2]
     ipR, portR = args[3]
     dStr = args[1]
     if isinstance(dStr, bytearray):
         dStr = toHexStr(dStr)
-    print('Received Data from {}:{} -> {}:{}\n : {}'.format(ipR, portR, ipL, portL, dStr))
+    print('Received Data from {}:{} -> {}:{}'.format(ipR, portR, ipL, portL))
+    print('[\x1B[92m{:>3}\x1B[39m] : \x1B[97m{}\x1B[0m'.format(_counter, dStr))
+
 
 def onSended(*args):
     ip,port = args[2]
@@ -44,6 +46,7 @@ def onSended(*args):
         dStr = toHexStr(dStr)
     print('Data has send to {}:{}\n :  {}'.format(ip, port, dStr))
 
+
 def onSendfail(*args):
     ip,port = args[2]
     dStr = args[1]
@@ -51,6 +54,7 @@ def onSendfail(*args):
         dStr = toHexStr(dStr)
     print('Send data to {}:{} fail\n : {}\n{}'.format(ip, port, dStr, arg[3]))
     print(traceback.format_exc())
+
 
 def createReceiver(*args):
     # args : Listen Port, Group Ip1, Group Ip2, ...
@@ -71,11 +75,13 @@ def createReceiver(*args):
     _rcv.joinGroup(args[idx:])
     _rcv.start()
 
+
 def stopReceiver(*args):
     global _rcv
     if _rcv is not None and _rcv.isAlive:
         _rcv.stop()
     _rcv = None
+
 
 def createSender(*args):
     global _snd
@@ -83,12 +89,15 @@ def createSender(*args):
     _snd.bind(key=EventTypes.SENDED, evt=onSended)
     _snd.bind(key=EventTypes.SENDFAIL, evt=onSendfail)
 
+
 def stopSender(*args):
     global _snd
     if _snd is not None:
         _snd = None
 
+
 def sendData(*args):
+    global _snd
     if len(args) < 3:
         print('Missing some arg')
     addr = (args[0], int(args[1]))
@@ -100,29 +109,30 @@ def sendData(*args):
         data = bytearray(data.encode('utf-8'))
     _snd.send(addr, data)
 
+
 def joinGroup(*args):
     _rcv.joinGroup(*(args))
+
 
 def dropGroup(*args):
     _rcv.dropGroup(*(args))
 
+
 def waitStdin():
     global _rcv
+    global _snd
     cmd = ''
     while cmd != 'exit':
         try:
-            if sys.version_info < (3, 0):
-                cmd = raw_input('Command: ')
-            else:
-                cmd = input('Command: ')
+            cmd = input('Command: ')
             if len(cmd) == 0:
                 continue
-            cmds = cmd.split( )
+            cmds = cmd.split()
             try:
                 if cmds[0] == 'exit':
-                    if _snd is not None:
+                    if _snd:
                         stopSender()
-                    if _rcv is not None:
+                    if _rcv:
                         stopReceiver()
                     break
                 elif cmds[0] == 'start':
@@ -131,9 +141,9 @@ def waitStdin():
                     elif cmds[1] == 'send':
                         createSender(*(cmds[2:]))
                 elif cmds[0] == 'stop':
-                    if cmds[1] == 'recv' and _rcv is not None:
+                    if cmds[1] == 'recv' and _rcv:
                         stopReceiver()
-                    elif cmds[1] == 'send' and _snd is not None:
+                    elif cmds[1] == 'send' and _snd:
                         stopSender()
                 elif cmds[0] == 'join':
                     joinGroup(*(cmds[1:]))
@@ -149,17 +159,6 @@ def waitStdin():
             break
     print
 
-def get_ip(iface = 'eth0'):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sockfd = sock.fileno()
-    SIOCGIFADDR = 0x8915
-    ifreq = struct.pack('16sH14s', iface, socket.AF_INET, '\x00'*14)
-    try:
-        res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
-    except:
-        return None
-    ip = struct.unpack('16sH2x4s8x', res)[2]
-    return socket.inet_ntoa(ip)
 
 if __name__ == '__main__':
     print('Host Name : {}'.format(socket.gethostname()))
