d181161e498b08932d2067baec96b1daca13b61a
怪頭 <jaofeng.chen@gmail.com>
2019-07-04 13:23:17 +0800

    send method add waitback param

--- jfNet/CastSender.py
+++ jfNet/CastSender.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 # # -*- coding: UTF-8 -*-
 
-import struct
-import socket
+import struct, socket
+from typing import Optional
 from . import EventTypes, SocketError
 
 
@@ -11,7 +11,7 @@ class CastSender:
     傳入參數:
         `evts` `dict{str:def,...}` -- 回呼事件定義，預設為 `None`
     """
-    def __init__(self, ttl: int = 8):
+    def __init__(self, ttl: int = 4):
         self.__events: dict = {
             EventTypes.SENDED: None,
             EventTypes.SENDFAIL: None
@@ -29,18 +29,19 @@ class CastSender:
             `TypeError` -- 型別錯誤，必須為可呼叫執行的函式
         """
         if key not in self.__events:
-            raise KeyError('key:\'{}\' not found!'.format(key))
+            raise KeyError(f'key:\'{key}\' not found!')
         if evt is not None and not callable(evt):
-            raise TypeError('evt:\'{}\' is not a function!'.format(evt))
+            raise TypeError(f'evt:\'{evt}\' is not a function!')
         self.__events[key] = evt
 
-    def send(self, remote:tuple, data):
+    def send(self, remote:tuple, data, waitback: bool = False) -> Optional[bytes]:
         """發送資料至多播位址
         傳入參數:
             `remote` `tuple(ip, port)` -- 多播位址
             `data` `str or bytearray` -- 欲傳送的資料
+            `waitback` `bool` -- 是否等待遠端回覆資料，最長等待 1 秒
         引發錯誤:
-            `jfSocket.SocketError` -- 遠端連線已斷開
+            `jfSocket.SocketError` -- 多播位址不正確
             `Exception` -- 回呼的錯誤函式
         """
         v = socket.inet_aton(remote[0])[0]
@@ -52,7 +53,7 @@ class CastSender:
         if isinstance(data, str):
             data = data.encode('utf-8')
             ba = bytearray(data)
-        elif isinstance(data, bytearray):
+        elif isinstance(data, bytearray) or isinstance(data, bytes):
             ba = data[:]
         try:
             self.__socket.sendto(ba, (remote[0], int(remote[1])))
@@ -62,3 +63,17 @@ class CastSender:
         else:
             if self.__events[EventTypes.SENDED]:
                 self.__events[EventTypes.SENDED](self, ba, remote)
+            if waitback:
+                sto = self.__socket.gettimeout()
+                self.__socket.settimeout(1)
+                try:
+                    rec, addr = self.__socket.recvfrom(1024)
+                except socket.timeout:
+                    pass
+                else:
+                    if rec and len(rec) != 0:
+                        return rec
+                    else:
+                        return None
+                finally:
+                    self.__socket.settimeout(sto)
